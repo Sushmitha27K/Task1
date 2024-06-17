75fb7ae59f47e7d25bb17bb55fd362d5d467fb16
Sushmitha27K <sushmitha2721.k@gmail.com>
2024-06-17 14:11:24 +0530

    Add files via upload

---
+++ E-Commerce1/src/main/java/com/example/E_Commerce1/repository/CategoryRepository.java
@@ -0,0 +1,7 @@
+package com.example.E_Commerce1.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface CategoryRepository extends JpaRepository<T, ID> {
+
+}
