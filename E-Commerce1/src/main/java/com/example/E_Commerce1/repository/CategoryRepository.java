716fd7cffd9c676bfb4e25fbd14d13fa9ed52ddf
Sushmitha27K <sushmitha2721.k@gmail.com>
2024-06-18 10:47:55 +0530

    Delete E-Commerce1 directory

--- E-Commerce1/src/main/java/com/example/E_Commerce1/repository/CategoryRepository.java
+++
@@ -1,7 +0,0 @@
-package com.example.E_Commerce1.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface CategoryRepository extends JpaRepository<T, ID> {
-
-}
